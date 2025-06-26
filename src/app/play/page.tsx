48a9a6d5eeaab58850814d5b9bc283a9cc880dfc
combikms <combikms@naver.com>
2025-06-26 16:04:20 +0900

    :sparkles: Feat: Routing

---
+++ src/app/play/page.tsx
@@ -0,0 +1,7 @@
+export default function PlayPage() {
+    return (
+        <main className="flex min-h-screen items-center justify-center">
+            <h1 className="text-3xl font-bold">🎮 Play Page</h1>
+        </main>
+    );
+}
